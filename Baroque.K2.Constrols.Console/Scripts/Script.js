a222328c94a0ba09be0ac139523bd722a2e0f76c
iAlexeyProkhorov <i.alexey.prokhorov@outlook.com>
2024-02-22 15:03:39 +0200

    Fix solution file name

--- Baroque.K2.Constrols.Console/Scripts/Script.js
+++ Baroque.K2.Constrols.Console/Scripts/Script.js
@@ -3,44 +3,44 @@
 
     BaroqueConsole = {
         getControlId: function (objInfo) {
-            var value = '#' + objInfo.CurrentControlId;
+            let value = '#' + objInfo.CurrentControlId;
             return value;
         },
         getInstance: function (objInfo) {
             return $(BaroqueConsole.getControlId(objInfo));
         },
         getOptions: function (objInfo) {
-            var controlId = BaroqueConsole.getControlId(objInfo);
-            var options = $(controlId).data("options");
+            let controlId = BaroqueConsole.getControlId(objInfo);
+            let options = $(controlId).data("options");
             if (options === undefined || options === null)
                 options = {};
             //console.log(controlId + " options:" + JSON.stringify(options));
             return options;
         },
         execute: function (objInfo) {
-            var methodName = objInfo.methodName;
-            var control = BaroqueConsole.getInstance(objInfo);
+            let methodName = objInfo.methodName;
+            let control = BaroqueConsole.getInstance(objInfo);
 
             switch (methodName) {
                 case 'clear':
                     console.clear();
                     break;
                 case 'count':
-                    var label = BaroqueConsole.getParameter(objInfo, "label");
+                    let label = BaroqueConsole.getParameter(objInfo, "label");
                     console.count(label);
                     break;
                 case 'error':
-                    var message = BaroqueConsole.getParameter(objInfo, "message");
+                    let message = BaroqueConsole.getParameter(objInfo, "message");
                     console.error(message);
                     control.trigger("OnMessageLogged");
                     break;
                 case 'group':
-                    var label = BaroqueConsole.getParameter(objInfo, "label");
+                    let label = BaroqueConsole.getParameter(objInfo, "label");
                     console.group(label);
                     control.trigger("OnGroupingStarted");
                     break;
                 case 'groupCollapsed':
-                    var label = BaroqueConsole.getParameter(objInfo, "label");
+                    let label = BaroqueConsole.getParameter(objInfo, "label");
                     console.groupCollapsed(label);
                     break;
                 case 'groupEnd':
@@ -48,31 +48,31 @@
                     control.trigger("OnGroupingEnded");
                     break;
                 case 'info':
-                    var message = BaroqueConsole.getParameter(objInfo, "message");
+                    let message = BaroqueConsole.getParameter(objInfo, "message");
                     console.info(message);
                     control.trigger("OnMessageLogged");
                     break;
                 case 'log':
-                    var message = BaroqueConsole.prepareMessage(objInfo, BaroqueConsole.getParameter(objInfo, "message"));
+                    let message = BaroqueConsole.prepareMessage(objInfo, BaroqueConsole.getParameter(objInfo, "message"));
                     console.log(message);
                     control.trigger("OnMessageLogged");
                     break;
                 case 'time':
-                    var label = BaroqueConsole.getParameter(objInfo, "label");
+                    let label = BaroqueConsole.getParameter(objInfo, "label");
                     console.time(label);
                     control.trigger("OnTimerStart");
                     break;
                 case 'timeEnd':
-                    var label = BaroqueConsole.getParameter(objInfo, "label");
+                    let label = BaroqueConsole.getParameter(objInfo, "label");
                     console.timeEnd(label);
                     control.trigger("OnTimerEnd");
                     break;
                 case 'trace':
-                    var label = BaroqueConsole.getParameter(objInfo, "label");
+                    let label = BaroqueConsole.getParameter(objInfo, "label");
                     console.trace(label);
                     break;
                 case 'warn':
-                    var message = BaroqueConsole.getParameter(objInfo, "message");
+                    let message = BaroqueConsole.getParameter(objInfo, "message");
                     console.warn(message);
                     control.trigger("OnMessageLogged");
                     break;
@@ -82,11 +82,11 @@
             }
         },
         getProperty: function (objInfo) {
-            var value = null;
-            var property = objInfo.property;
-            var options = BaroqueConsole.getOptions(objInfo);
+            let value = null;
+            let property = objInfo.property;
+            let options = BaroqueConsole.getOptions(objInfo);
 
-            for (var name in options)
+            for (let name in options)
                 if (name.toLowerCase() == property.toLowerCase()) {
                     value = options[name];
                     break;
@@ -94,39 +94,39 @@
             return value;
         },
         setProperty: function (objInfo) {
-            var controlId = BaroqueConsole.getControlId(objInfo);
-            var options = BaroqueConsole.getOptions(objInfo);
+            let controlId = BaroqueConsole.getControlId(objInfo);
+            let options = BaroqueConsole.getOptions(objInfo);
             //console.log("TemporaryStrorage.setProperty:" + JSON.stringify(options));
             options[property] = value;
             $(controlId).data("options", options);
         },
         getValue: function (objInfo) {
             //console.log("getValue: " + JSON.stringify(objInfo));
-            var options = BaroqueConsole.getOptions(objInfo);
+            let options = BaroqueConsole.getOptions(objInfo);
             //console.log("getValue.options: " + JSON.stringify(options));
-            var value = options["Value"];
+            let value = options["Value"];
             return value;
         },
         setValue: function (objInfo) {
-            var controlId = BaroqueConsole.getControlId(objInfo);
-            var options = BaroqueConsole.getOptions(objInfo);
+            let controlId = BaroqueConsole.getControlId(objInfo);
+            let options = BaroqueConsole.getOptions(objInfo);
             //console.log("setValue: " + JSON.stringify(objInfo));
             options["Value"] = objInfo.Value;
             $(controlId).data("options", options);
         },
         getParameter: function (objInfo, name) {
-            var value = objInfo.methodParameters[name];
+            let value = objInfo.methodParameters[name];
             return value;
         },
         prepareMessage: function (objInfo, value) {
-            var options = BaroqueConsole.getOptions(objInfo);
+            let options = BaroqueConsole.getOptions(objInfo);
             if (BaroqueConsole.isEmpty(options["DisplayLogTime"])) {
                 return value;
             }
 
-            var displayLogTimeValue = options["DisplayLogTime"];
+            let displayLogTimeValue = options["DisplayLogTime"];
             if (displayLogTimeValue == 'true') {
-                var now = new Date($.now());
+                let now = new Date($.now());
                 value = now.getMinutes() + ":" + now.getSeconds() + ":" + now.getMilliseconds() + " || " + value;
             }
             return value;
